2f8d83d27783b563eeb723676b6dbcd0f1403bf0
ilhankaradag <ilhankaradag932@gmail.com>
2023-11-04 12:40:26 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const port = 7000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is now listening on port ${port}`);
+});
